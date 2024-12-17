c774528f915d773e75a2738c59cbcc6505381f7c
Igor Babic <ibabicit22@gmail.com>
2024-12-17 10:43:17 +0100

    added assets files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,73 @@
+import { useRef } from "react";
+import { grapevideo } from "../assets";
+
+import Button from "./Button";
+import Section from "./Section";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+
+const Hero = () => {
+  const parallaxRef = useRef(null);
+
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25rem]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative" ref={parallaxRef}>
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+          <h1 className="h1 mb-6">
+            Explore Chat Possibilities of&nbsp;Any URL on the web&nbsp; with{" "}
+            {` `}
+            <span className="inline-block relative">Grapevine </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum a
+            nostrum veritatis expedita iste.
+          </p>
+          <Button
+            onClick={() =>
+              window.open(
+                "https://chromewebstore.google.com/detail/grapevine/pplalkgaganjjijekcoikebmnjfdhfbf",
+                "_blank"
+              )
+            }
+            white
+          >
+            Get started
+          </Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <video
+                  autoPlay
+                  muted
+                  loop
+                  src={grapevideo}
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  width={1024}
+                  height={490}
+                  alt="AI"
+                />
+              </div>
+            </div>
+
+            <Gradient />
+          </div>
+
+          <BackgroundCircles />
+        </div>
+      </div>
+
+      <BottomLine />
+    </Section>
+  );
+};
+
+export default Hero;
